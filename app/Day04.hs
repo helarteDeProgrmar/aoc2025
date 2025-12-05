94190cb68ac94bda4e77462340a6e17d74564bf5
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-12-05 13:57:56 +0100

    feat: completed day 4, incredible horrible solution

--- app/Day04.hs
+++ app/Day04.hs
@@ -5,11 +5,10 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  content <- readFile "inputs/day04-2.txt"
+  content <- readFile "inputs/day04.txt"
   let input = build content
   print $ resolve1 input
-  print $ resolve1_aux input
-  print $ resolve2 input
+  print $ resolve2 input 1
 
 build :: String -> Array (Int, Int) Char
 build s =
@@ -24,11 +23,9 @@ build s =
     rows = length ls
     columns = length $ head ls
 
-resolve1_aux :: Array (Int, Int) Char -> Int
-resolve1_aux arr = length $ [idx | idx <- indices arr, arr ! idx /= '@']
 
 resolve1 :: Array (Int, Int) Char -> Int
-resolve1 arr = length $ filter (< 4) $ trace (show $ length numbers_role) numbers_role
+resolve1 arr = length $ filter (< 4) numbers_role
   where
     numbers_role = [count_adjacents_rolls arr idx | idx <- indices arr, arr ! idx == '@']
 
@@ -36,8 +33,16 @@ count_adjacents_rolls :: Array (Int, Int) Char -> (Int, Int) -> Int
 count_adjacents_rolls arr (a, b) = length $ filter (== '@') [arr ! i | i <- adjacents_idx]
   where
     adjacents_idx = [(i, j) | i <- [a - 1 .. a + 1], j <- [b - 1 .. b + 1], (i, j) /= (a, b), in_bounds (i, j)]
-    in_bounds (i, j) = i > 0 && j > 0 && i < rows && j < cols
+    in_bounds (i, j) = i > 0 && j > 0 && i <= rows && j <= cols
     (_, (rows, cols)) = bounds arr
 
-resolve2 :: Array (Int, Int) Char -> Int
-resolve2 _ = 1
+resolve2 :: Array (Int, Int) Char -> Int -> Int
+resolve2 arr 0 = -1
+resolve2 arr n = n + (resolve2 (transform arr) $ resolve1 arr)
+
+transform :: Array (Int, Int) Char -> Array (Int, Int) Char
+transform arr = array
+                  (bounds arr)
+                  [ ((i, j), decide_char (i,j)) | (i,j) <- indices arr]
+                where
+                  decide_char idx = if count_adjacents_rolls arr idx < 4 then '.' else arr ! idx

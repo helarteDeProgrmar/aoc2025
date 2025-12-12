ed5eeff5bb5c5b31c58db3076cdf0a28aaf2d291
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-12-12 15:59:14 +0100

    feat: fisrt part of day 7

--- app/Day07.hs
+++ app/Day07.hs
@@ -1,19 +1,28 @@
 module Day07 where
 
+import Debug.Trace (trace)
 main :: IO ()
 main = do
-  content <- readFile "inputs/day07-2.txt"
-  let (start, continue) = lines content
-  print $ resolve1 start continue
+  content <- readFile "inputs/day07.txt"
+  let (start:continue) = lines content
+  print $ resolve1 start continue 0
+  print $ resolve2 start continue 0
 
-resolve1 :: String -> [String] -> Int
-resolve1 current [] = length $ filter (\x -> x == '|') current
-resolve1 current (x:xs) = resolve1 (next current x) xs
-
-next :: String -> String
-next current next = [prepare_char i | i <- [0 .. length current - 1]]
+resolve1 :: String -> [String] -> Int -> Int
+resolve1 _ [] total = total
+resolve1 current (x:xs) acc = resolve1 next_gen xs (acc + bif)
   where
+    (next_gen, bif) = next current x
+
+next :: String -> String -> (String, Int)
+next current next_gen = trace (show sol) sol
+  where
+    sol = ([prepare_char i | i <- [0 .. len - 1]], length [i | i<- [0.. len - 1], current !! i == '|', next_gen !! i == '^'])
+    len = length current
     prepare_char i
-      | current !! i == '^' = '.'
-      | next !! i - 1 == '^' && i
-      | current !! i + 1 == '^'
+      | next_gen !! i == '^' = '^'
+      | (i-1) >= 0 && next_gen !! (i-1) == '^' && current !! (i-1) == '|' = '|'
+      | (i+1) < len && next_gen !! (i+1) == '^' && current !! (i+1) == '|' = '|'
+      | current !! i == 'S' = '|'
+      | current !! i == '|' = '|'
+      | otherwise = '.'

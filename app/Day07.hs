bbf1372535c5ad69fa0ba5f60e6a37b0e8f5992a
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-12-12 17:28:45 +0100

    feat: incredible solution for day 7

--- app/Day07.hs
+++ app/Day07.hs
@@ -1,12 +1,14 @@
 module Day07 where
 
-import Debug.Trace (trace)
+import Data.List (elemIndex)
+
 main :: IO ()
 main = do
   content <- readFile "inputs/day07.txt"
   let (start:continue) = lines content
   print $ resolve1 start continue 0
-  print $ resolve2 start continue 0
+  let counts = map (\x -> if x then 1 else 0) [start !! i == 'S' | i <- [0..length start-1]]
+  print $ resolve2 counts continue
 
 resolve1 :: String -> [String] -> Int -> Int
 resolve1 _ [] total = total
@@ -15,9 +17,8 @@ resolve1 current (x:xs) acc = resolve1 next_gen xs (acc + bif)
     (next_gen, bif) = next current x
 
 next :: String -> String -> (String, Int)
-next current next_gen = trace (show sol) sol
+next current next_gen = ([prepare_char i | i <- [0 .. len - 1]], length [i | i<- [0.. len - 1], current !! i == '|', next_gen !! i == '^'])
   where
-    sol = ([prepare_char i | i <- [0 .. len - 1]], length [i | i<- [0.. len - 1], current !! i == '|', next_gen !! i == '^'])
     len = length current
     prepare_char i
       | next_gen !! i == '^' = '^'
@@ -26,3 +27,19 @@ next current next_gen = trace (show sol) sol
       | current !! i == 'S' = '|'
       | current !! i == '|' = '|'
       | otherwise = '.'
+
+resolve2 counts [] = sum counts
+resolve2 counts (s:ss) = resolve2 [left_add i + mid_add i + right_add i| i <- [0..len -1]] ss
+  where
+    len = length counts
+    mid_add i
+      | s !! i == '^' = 0
+      | otherwise = counts !! i
+    left_add i
+      | i == 0 = 0
+      | s !! (i - 1) == '^' = counts !! (i-1)
+      | otherwise = 0
+    right_add i
+      | i == len - 1 = 0
+      | s !! (i + 1) == '^' = counts !! (i+1)
+      | otherwise = 0
